c16660c3bab5e4f5988c6f8df4dadbee0ec7587e
Oksana <oksanashukh@gmail.com>
2018-08-19 19:15:03 +0300

    created gulpfile

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -3,9 +3,9 @@ var sass = require('gulp-sass');
 var rigger = require('gulp-rigger');
  
 gulp.task('sass', function () {
-  return gulp.src('app/styles/main.scss')
+  return gulp.src('app/styles/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('app/css/'));
+    .pipe(gulp.dest('app/css'));
 });
 
 
@@ -17,7 +17,7 @@ gulp.task('html', function () {
 
  
 gulp.task('watch', function () {
-  gulp.watch('app/styles/**/*.scss', ['sass']);
+  gulp.watch('app/styles/*.scss', ['sass']);
   gulp.watch('app/*.html', ['html']);
 });
 
